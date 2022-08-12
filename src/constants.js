0275dd4fc97181aa21646e5ff42346bcc33bbe05
hamaup <hamaup@gmail.com>
2022-08-12 19:00:59 +0900

    upload to github

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+// constants.js
+const CONTRACT_ADDRESS = "0x9bB1e9d9d36BF8569CB48687ED85507B771E2d2B";
+
+//NFT キャラクター の属性をフォーマットしてオブジェクトとして返します。
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+// 変数を constants.js 以外の場所でも使えるようにします。
+export { CONTRACT_ADDRESS, transformCharacterData };
